92ea02216fab17b7cf14f34f1d94c460e3a9237f
Jhanvi Desai <jdesai024@gmail.com>
2024-07-18 15:09:22 +0530

    integrate chatbot

--- chatbot/src/pages/home/home.component.ts
+++ chatbot/src/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
 
